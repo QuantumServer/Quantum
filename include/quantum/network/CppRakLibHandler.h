6190d53eb58e5ef1e335de81488a11d625a13770
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-20 22:16:39 -0700

    Update CppRakLibHandler.h

--- include/quantum/network/CppRakLibHandler.h
+++ include/quantum/network/CppRakLibHandler.h
@@ -4,7 +4,7 @@
 
 #include "RakPeerInterface.h"
 
-class RakNetHandler {
+class CppRakLibHandler {
 
     public:
         void Initialize(int port, std::string ip, int max_players, std::string server_name);
