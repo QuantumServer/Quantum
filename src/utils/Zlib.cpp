382d8bde7ed29e1d85737ea3bfdd00dd282bbf78
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-20 20:41:30 -0700

    zlib compress and decompress

---
+++ src/utils/Zlib.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <zlib.h>
+
+#include "util/Zlib.h"
+
+#define BUFLEN 17000
+Byte uncompr[BUFLEN];
+
+std::string Zlib::Decompress(unsigned char const& data) {
+
+	uLongf length = BUFLEN;
+	uncompress(uncompr, &length, (Bytef*) data, sizeof(data));
+	return std::string((char*) uncompr, length);
+
+}
