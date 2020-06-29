2665a9531fddb79091bd3d417de79f0f1c76842e
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-28 23:31:16 -0700

    Create Utils.cpp

---
+++ src/utils/Utils.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#if defined(_WIN32) || (_WIN64)
+#define PLATFORM_NAME "windows"
+#elif defined (__linux__)
+#define PLATFORM_NAME "linux"
+#elif defined (__APPLE__)
+#define PLATFORM_NAME "osx"
+#endif
+
+const char getOS() {
+    return PLATFORM_NAME;
+}
