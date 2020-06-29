9046327e6bf74a631130ca774f1d2d077f4c608a
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-28 19:33:19 -0700

    Rewrite main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,3 @@
-#include "Config.h"
-
-#include <iostream>
-#include <locale>
-#include <string>
-#include <vector>
-#include <boost/fiber/algo/asio.hpp>
-#include <boost/fiber/asio/use_future.hpp>
-#include <boost/filesystem/path.hpp>
-#include <boost/locale/format.hpp>
-#include <boost/locale/generator.hpp>
-#include <boost/locale/message.hpp>
-#include <boost/program_options.hpp>
 #include <stdio.h>
 #include <thread>
 
@@ -18,62 +5,14 @@
 #include "Server.h"
 
 void start() {
-
     Server::initialize();
-
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
-    boost::locale::generator localeGen;
-    localeGen.add_messages_path(PACKAGE_LOCALE_DIR);
-    localeGen.set_default_messages_domain(GETTEXT_PACKAGE);
-    boost::program_options::options_description desc;
-    std::string configFile;
+    printf("Starting Katana...\n");
 
-    desc.add_options()(
-        "help,h",
-        boost::locale::translate("display this help and exit").str().c_str()
-    )(
-        "version,V",
-        boost::locale::translate("output version information and exit")
-        .str()
-        .c_str()
-    )(
-        "config,c",
-        boost::program_options::value<std::string>(&configFile)
-            ->value_name(boost::locale::translate("configuration file"))
-            ->default_value(CONFIG_DIR "/cenisys.conf"),
-            boost::locale::translate("set the configuration file").str().c_str()
-    );
-
-    printf("Starting Quantum...\n");
     std::thread server(start);
     server.join();
-    boost::program_options::variables_map vm;
-
-    boost::program_options::store(
-        boost::program_options::parse_command_line(argc, argv, desc), vm
-    );
-
-    boost::program_options::notify(vm);
-
-    if (vm.count("help")) {
-        std::cout << desc;
-        return 0;
-    }
-
-    if (vm.count("version")) {
-        std::locale oldCoutLoc = std::cout.imbue(localeGen(""));
-        std::cout
-            << boost::locale::format(
-                boost::locale::translate("Cenisys {1}\n")
-            ) % SERVER_VERSION
-        std::cout.imbue(oldCoutLoc);
-        return 0;
-	}
-
-    Cenisys::ServerManager manager(configFile);
-    return manager.exec();
 
 }
