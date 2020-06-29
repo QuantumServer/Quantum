98974dd3f74a8f0053b9f0d2d20c781fe233efa0
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-28 19:35:28 -0700

    Update Server.cpp

--- src/Server.cpp
+++ src/Server.cpp
@@ -11,8 +11,8 @@
 
 void Server::initialize() {
     std::cout << TextFormat::GOLD << " " << std::end1;
-    std::cout << TextFormat::BLUE << "Starting server"
-    std::cout << TextFormat::BLUE << "Quantum is licensed under Apache License 2.0"
+    std::cout << TextFormat::BLUE << "Starting server";
+    std::cout << TextFormat::BLUE << "Quantum is licensed under Apache License 2.0";
 
     Server::bind(19132, "0.0.0.0", 20, "Quantum powered server");
 }
@@ -20,7 +20,7 @@ void Server::initialize() {
 void Server::bind(int port, std::string ip, int max_players, std::string server_name) {
     std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
     RakNetHandler* handler;
-    handler->initialize(port, ip_addr, max_players, server_name);
+    handler->initialize(port, ip, max_players, server_name);
     std::chrono::high_resolution_clock::time_point end_time = std::chrono::high_resolution_clock::now();
     long exec_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
     printf("Server Invoked, Took %ldms\n", exec_time);
@@ -31,6 +31,6 @@ namespace quantum {
 
     Server::Server(Server::Config &config) : config_(config) {
 
-	}
+    }
 
 }
