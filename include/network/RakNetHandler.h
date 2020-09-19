c2fa8a3479603e8defd9f9b04829226f3308331b
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-09-18 21:50:30 -0700

    Update file path

--- include/network/RakNetHandler.h
+++ include/network/RakNetHandler.h
@@ -4,7 +4,7 @@
 
 #include "RakPeerInterface.h"
 
-class CppRakLibHandler {
+class RakNetHandler {
 
     public:
         void Initialize(int port, std::string ip, int max_players, std::string server_name);
