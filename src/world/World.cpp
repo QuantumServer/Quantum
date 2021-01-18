9c0b4fa3a2a5b5538869dc5bae6f39446c086bfa
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-01-17 17:28:17 -0800

    Update World.cpp

--- src/world/World.cpp
+++ src/world/World.cpp
@@ -1,1 +1,12 @@
+#include "World.h"
+#include "Chunk.h"
+// #include "math/Vector3.h"
 
+/*
+World::World(std::vector<Chunk>* chunks, Vector3* spawn_location):chunks{chunks}, spawnLocation{spawnLocation} {
+
+}
+
+World::~World() {
+
+}*/
