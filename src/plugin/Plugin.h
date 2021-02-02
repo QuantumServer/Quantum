a305c56c4bb72a8db25e3e81172d19ef8c0aad20
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-02-01 19:55:53 -0800

    Create Plugin.h

---
+++ src/plugin/Plugin.h
@@ -0,0 +1,12 @@
+#include "Server.h"
+
+class Plugin {
+
+public:
+    std::string name = "";
+    std::string description = "";
+    std::string author = "";
+    std::string version = "";
+    Server server;
+
+}
